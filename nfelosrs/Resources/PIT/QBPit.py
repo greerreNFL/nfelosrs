618a00a3bedc8787a2b2684ed1d941918a0dd6b7
Robert Greer <robertgreer@Roberts-Mac-Studio.local>
2024-08-18 12:20:53 -0700

    updated qb game merge logic and new 2024 WTs

--- nfelosrs/Resources/PIT/QBPit.py
+++ nfelosrs/Resources/PIT/QBPit.py
@@ -19,50 +19,21 @@ class QBPit:
         self.season = season
         self.week = week
         ## format and transform ##
-        self.add_game_id()
         self.filter_to_season_week()
         self.qb_df_flat = self.flatten_qbs()
         self.recent_rankings = self.get_most_recent_rankings()
         ## calculate ##
         self.weekly_qb_adjustments = self.calc_adjs()
-    
-    def add_game_id(self):
-        '''
-        Adds the nflfastr game ids to the qbelo file
-        '''
-        ## create a datetime series from the date ##
-        self.qb_df['date_time'] = pd.to_datetime(self.qb_df['date'])
-        ## mondays are new weeks, so subtract a day and then trunc to get an NFL week ##
-        self.qb_df['date_time'] = self.qb_df['date_time'] - pd.Timedelta(days=1)
-        self.qb_df['week_of'] = self.qb_df['date_time'].dt.isocalendar().week
-        self.qb_df = self.qb_df.sort_values(
-            by=['date_time'],
-            ascending=[True]
-        ).reset_index(drop=True)
-        ## get unique weeks ##
-        unique_week_df = self.qb_df.copy()[['season','week_of']].drop_duplicates()
-        unique_week_df['week'] = unique_week_df.groupby(['season']).cumcount() + 1
-        self.qb_df = pd.merge(
-            self.qb_df,
-            unique_week_df,
-            on=['season','week_of'],
-            how='left'
-        )
-        ## create nflfastR style game id ##
-        self.qb_df = pd.merge(
-            self.qb_df.rename(columns={
-                'team1' : 'home_team',
-                'team2' : 'away_team'
-            }),
-            self.games[['season', 'week', 'home_team', 'away_team', 'game_id']],
-            on=['season', 'week', 'home_team', 'away_team'],
-            how='left'
-        )
 
     def filter_to_season_week(self):
         '''
-        Filters the QB file down to the most recent week
+        Filters the QB file down to the most recent week and provides
+        minor formatting
         '''
+        self.qb_df=self.qb_df.rename(columns={
+            'team1' : 'home_team',
+            'team2' : 'away_team'
+        })
         self.qb_df = self.qb_df[
             (self.qb_df['season'] == self.season) &
             (self.qb_df['week'] <= self.week)
